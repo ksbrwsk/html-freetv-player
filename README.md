f1d8f7ceeff80ba95685ede893b4712352e76e54
Kai Saborowski <kai.saborowski@googlemail.com>
2023-11-02 19:01:31 +0100

    Polished.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # html-freetv-player
-simple html5 german free tv player
+#### simple html5 german free tv player
+
+![Java CI with Maven](https://github.com/ksbrwsk/html-freetv-player/workflows/Java%20CI%20with%20Maven/badge.svg)
+
+**Prerequisites:**
+* [Java 21](https://openjdk.net/)
+* [Apache Maven](https:http://maven.apache.org/)
+
+> ⚠️ **Currently only working on Apple Safari, no Chrome or Edge support yet!**
+
+**Themes:**
+* Spring MVC
+* Thymeleaf
+* Bootstrap
+
+#### How to build and run
+Type
+```bash
+mvn package
+mvn spring-boot:run
+```
+to build and run the application on your local environment.
+
+Point your browser to
+```bash
+http://localhost:8080
+```
+to try out the player.
--- src/main/java/de/ksbrwsk/tvplayer/HtmlFreeTvPlayerController.java
+++ src/main/java/de/ksbrwsk/tvplayer/HtmlFreeTvPlayerController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,6 +20,7 @@ import java.util.List;
 public class HtmlFreeTvPlayerController {
     private static final String PAGE_INDEX = "index";
     private static final String PAGE_START = "start";
+    private static final String PAGE_CHANNELS = "channels";
 
     private final TvChannelRepository tvChannelRepository;
 
@@ -31,6 +33,23 @@ public class HtmlFreeTvPlayerController {
         model.addAttribute("tvChannels", listTvChannel);
         model.addAttribute("tvChannel", tvChannelForm);
         return PAGE_INDEX;
+    }
+
+    @GetMapping(value = {"/channels"})
+    public String channels(Model model) {
+        addCommonModelAttributes(model);
+        List<TvChannel> listTvChannel = this.tvChannelRepository.getListTvChannel();
+        model.addAttribute("tvChannels", listTvChannel);
+        return PAGE_CHANNELS;
+    }
+
+    @GetMapping(value = {"/start"})
+    public String start(Model model, @NotNull @RequestParam("id") Long id) {
+        addCommonModelAttributes(model);
+        TvChannel tvChannel = this.tvChannelRepository.getTvChannel(id);
+        model.addAttribute("tvChannelUrl", tvChannel.getUrl());
+        log.info("requested tv station -> {}-{}", tvChannel.getId(), tvChannel.getName());
+        return PAGE_START;
     }
 
     @PostMapping(value = "/start")
